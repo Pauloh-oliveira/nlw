c03a291b9700edf13e71a4c768b187233b43a313
Paulo Henrique <paulohenrique_789@hotmail.com>
2020-08-10 22:04:09 -0300

    Finalizando a integração do banco de dados com os dados do formulario

--- src/server.js
+++ src/server.js
@@ -1,77 +1,19 @@
-const proffys = [
-    {name: 'Paulo Henrique',
-     avatar: "https://avatars1.githubusercontent.com/u/47150535?s=460&v=4" ,
-     whatsapp: '4199999999',
-     bio : 'Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.', 
-     subject : 'Química', 
-     cost : '60', 
-     weekday: [
-         0
-     ], 
-     time_from: [720] , 
-     time_to:[1220] }
-]
-
-const subjects = [
-    "Artes",
-    "Biologia",
-    "Ciências",
-   "Educação física",
-    "Física",
-    "Geografia",
-    "História",
-    "Matemática",
-    "Português",
-    "Química",
-]
-
-const weekdays = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado",
-]
-
+const { pageLanding, pageStudy, pageGiveClasses} = require('./pages')
 const express = require('express')
 const server = express()
 const nunjucks = require('nunjucks')
-
 nunjucks.configure('src/views', {
     express: server,
     noCache: true
 })
 
-function getSubject(subjectNumber) {
-    const position = +subjectNumber -1
-    return subjects[position]
-}
+const { saveClasses} = require("./pages")
 
-function pageLanding(req, res){
-    return res.render('index.html')
-}
-
-function pageStudy(req, res) {
-    const filters = req.query
-    return res.render('study.html', { proffys, filters, subjects, weekdays})
-}
-
-function pageGiveClasses(req, res) {
-    const data = req.query
-    const isNotEmpty = Object.keys(data).length > 0
-    if (isNotEmpty) {
-        data.subject = getSubject(data.subject)
-        proffys.push(data)
-        return res.redirect("/study")
-    }
-
-    return res.render('give-classes.html', { subjects, weekdays})
-}
 
 server.use(express.static("public"))
+.use(express.urlencoded({ extended: true}))
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
 .listen(5500)
